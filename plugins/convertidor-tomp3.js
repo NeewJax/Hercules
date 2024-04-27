b1d57cca11dbf0fd509184b9d2d8f7d77869c61d
NeewJax <gabrielduarte.gomes09@gmail.com>
2024-04-26 23:50:36 -0300

    new sticker lib and wm fixed reply stickers!

--- plugins/convertidor-tomp3.js
+++ plugins/convertidor-tomp3.js
@@ -1,14 +1,15 @@
-import { toAudio } from '../lib/converter.js'
-let handler = async (m, { conn, usedPrefix, command }) => {
-let q = m.quoted ? m.quoted : m
-let mime = (q || q.msg).mimetype || q.mediaType || ''
-if (!/video|audio/.test(mime)) throw `*[❗𝐈𝐍𝐅𝐎❗] Responda o video ou mensagem de voz que você deseja converter em áudio/MP3*`
-let media = await q.download()
-if (!media) throw '*[❗𝐈𝐍𝐅𝐎❗] Peço desculpas, ocorreu um erro ao baixar o seu vídeo, você poderia tentar novamente? :(*'
-let audio = await toAudio(media, 'mp4')
-if (!audio.data) throw '*[❗𝐈𝐍𝐅𝐎❗] Peço desculpas, ocorreu um erro ao converter sua mensagem de voz a audio/mp3, você poderia tentar novamente? :(*'
-conn.sendMessage(m.chat, { audio: audio.data,  mimetype: 'audio/mpeg' }, { quoted: m })
-}
-handler.alias = ['tomp3', 'toaudio']
-handler.command = /^to(mp3|audio)$/i
-export default handler
+import {toAudio} from '../lib/converter.js';
+
+const handler = async (m, {conn, usedPrefix, command}) => {
+    const q = m.quoted ? m.quoted : m;
+    const mime = (q || q.msg).mimetype || q.mediaType || '';
+    if (!/video|audio/.test(mime)) throw '*You need to reply to a video or audio message to convert it to audio.*';
+    const media = await q.download();
+    if (!media) throw '*Failed to download the media.*';
+    const audio = await toAudio(media, 'mp4');
+    if (!audio.data) throw '*Failed to convert the media to audio.*';
+    conn.sendMessage(m.chat, {audio: audio.data, mimetype: 'audio/mpeg'}, {quoted: m});
+  };
+  handler.alias = ['tomp3', 'toaudio'];
+  handler.command = /^to(mp3|audio)$/i;
+  export default handler;
