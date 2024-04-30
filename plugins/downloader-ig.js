9432067c4d59d8499c82458b13aeca686901cf17
NeewJax <gabrielduarte.gomes09@gmail.com>
2024-04-29 22:49:01 -0300

    modified:   package.json 	modified:   plugins/downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,34 +1,37 @@
-import fetch from 'node-fetch'
-import axios from 'axios'
-import cheerio from 'cheerio'
-import instagramGetUrl from 'instagram-url-direct'
-import vm from 'node:vm'
-import { instagram } from "@xct007/frieren-scraper";
-import { instagramdl, instagramdlv2, instagramdlv3, instagramdlv4 } from '@bochilteam/scraper'
+import fetch from 'node-fetch';
+import puppeteer from 'puppeteer';
+import axios from 'axios';
+
 let handler = async (m, { conn, args, command, usedPrefix }) => {
-    if (!args[0]) throw `*[❗𝐈𝐍𝐅𝐎❗] Insira um link do instagram. Exemplo: ${usedPrefix + command}* https://www.instagram.com/reel/Cc0NuYBg8CR/?utm_source=ig_web_copy_link`
-    m.reply(`*✅ Baixando seu vídeo, por favor aguarde um momento... este processo pode durar entre 2 e 10 minutos dependendo da duração do seu vídeo...*`)
-                    try {
-                        let human = await fetch(`https://api.lolhuman.xyz/api/instagram?apikey=${lolkeysapi}&url=${args[0]}`)
-                        let json = await human.json()
-                        let videoig = json.result
-                        let shortUrl1 = await (await fetch(`https://tinyurl.com/api-create.php?url=${args[0]}`)).text()
-                        let txt1 = `🔗 *Url:* ${shortUrl1}`.trim()
-                        await conn.sendFile(m.chat, videoig, 'error.mp4', txt1, m)
-                    } catch {
-                        console.log('A tentativa 4 falhou, sem resultados')
-                        throw `*[❗𝐈𝐍𝐅𝐎❗] Erro... você poderia tentar novamente? :(*`
-                    } 
-}
-handler.command = /^(instagramdl|instagram|igdl|ig|instagramdl2|instagram2|igdl2|ig2|instagramdl3|instagram3|igdl3|ig3)$/i
-export default handler
-async function savefrom(urlL) {
-    let body = new URLSearchParams({ "sf_url": encodeURI(urlL), "sf_submit": "", "new": 2, "lang": "id", "app": "", "country": "id", "os": "Windows", "browser": "Chrome", "channel": " main", "sf-nomad": 1 });
-    let { data } = await axios({ "url": "https://worker.sf-tools.com/savefrom.php", "method": "POST", "data": body, "headers": { "content-type": "application/x-www-form-urlencoded", "origin": "https://id.savefrom.net", "referer": "https://id.savefrom.net/", "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36" } });
-    let exec = '[]["filter"]["constructor"](b).call(a);';
-    data = data.replace(exec, `\ntry {\ni++;\nif (i === 2) scriptResult = ${exec.split(".call")[0]}.toString();\nelse (\n${exec.replace(/;/, "")}\n);\n} catch {}`);
-    let context = { "scriptResult": "", "i": 0 };
-    vm.createContext(context);
-    new vm.Script(data).runInContext(context);
-    return JSON.parse(context.scriptResult.split("window.parent.sf.videoResult.show(")?.[1].split(");")?.[0])
-}
+    if (!args[0]) throw `*[❗𝐈𝐍𝐅𝐎❗] Insira um link do instagram. Exemplo: ${usedPrefix + command}* https://www.instagram.com/reel/Cc0NuYBg8CR/?utm_source=ig_web_copy_link`;
+    m.reply(`*✅ Obtendo link de download, por favor aguarde um momento...*`);
+
+    try {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto(`http://thejaxapi.rf.gd/instagram/insta-downloader.php?link=${args[0]}`);
+
+        // Espera até que o elemento com o link de download esteja disponível na página
+        await page.waitForSelector('pre', { timeout: 60000 }); // Aumentando o tempo limite para 60 segundos
+
+        // Obtém o link de download diretamente do texto dentro do elemento 'pre'
+        const downloadLink = await page.$eval('pre', element => {
+            const json = JSON.parse(element.textContent);
+            return json.data.download;
+        });
+
+        await browser.close();
+
+        // Baixa o vídeo usando o link obtido
+        const response = await axios.get(downloadLink, { responseType: 'arraybuffer' });
+
+        // Envie o vídeo baixado para o usuário
+        await conn.sendFile(m.chat, Buffer.from(response.data), 'video.mp4', '', m);
+    } catch (error) {
+        console.error('Erro ao baixar vídeo:', error);
+        throw `*[❗𝐈𝐍𝐅𝐎❗] Erro ao baixar o vídeo, por favor tente novamente :(*`;
+    }
+};
+
+handler.command = /^(instagramdl|instagram|igdl|ig|instagramdl2|instagram2|igdl2|ig2|instagramdl3|instagram3|igdl3|ig3)$/i;
+export default handler;
