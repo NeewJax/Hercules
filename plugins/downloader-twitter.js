e41f5dfcf3e3e8a5b5f7405dcaab2a0fc70c9260
NeewJax <gabrielduarte.gomes09@gmail.com>
2024-05-13 23:35:37 -0300

    fixing api twitter

--- plugins/downloader-twitter.js
+++ plugins/downloader-twitter.js
@@ -1,24 +1,26 @@
 import fetch from 'node-fetch';
+import puppeteer from 'puppeteer';
 import axios from 'axios';
 
 let handler = async (m, { conn, args, command, usedPrefix }) => {
     if (!args[0]) throw `*[❗𝐈𝐍𝐅𝐎❗] Insira um link do Twitter. Exemplo: ${usedPrefix + command}* https://twitter.com/auronplay/status/1586487664274206720?s=20&t=3snvkvwGUIez5iWYQAehpw`;
-    
+    m.reply(`*✅ Obtendo link de download, por favor aguarde um momento...*`);
     try {
-        m.reply(`*✅ Baixando seu vídeo, por favor aguarde um momento...*`);
+        const browser = await puppeteer.launch({ args: ['--no-sandbox'] }); // using --no-sandbox to launch on aws instance
+        const page = await browser.newPage();
+        await page.goto(`http://thejaxapi.rf.gd/api/twitter/video-downloader.php?link=${args[0]}`);
 
-        const apiUrl = `https://api.lolhuman.xyz/api/twitter?apikey=${lolkeysapi}&url=${args[0]}`;
-        const response = await fetch(apiUrl);
+        // Espera até que o elemento com o link de download esteja disponível na página
+        await page.waitForSelector('pre', { timeout: 60000 }); // Aumentando o tempo limite para 60 segundos
 
-        if (!response.ok) {
-            throw new Error('Erro ao acessar a API');
-        }
+        // Obtém o link de download diretamente do texto dentro do elemento 'pre'
+        const downloadLink = await page.$eval('pre', element => {
+            const json = JSON.parse(element.textContent);
+            return json.data.download;
+        });
 
-        const jsonData = await response.json();
+        await browser.close();
 
-        if (!jsonData.result) {
-            throw new Error('Erro: nenhum resultado encontrado na resposta da API');
-        }
 
         const tweetData = jsonData.result;
         const tweetVideoUrl = tweetData.media[0].url;
