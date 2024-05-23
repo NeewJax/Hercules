b2b98c1e7794a58d0845017f826ba41744624996
NeewJax <gabrielduarte.gomes09@gmail.com>
2024-05-22 23:32:27 -0300

    GPT + bot IA implemented

--- plugins/herramientas-chatGPT2.js
+++ plugins/herramientas-chatGPT2.js
@@ -1,56 +1,30 @@
-/*-------------------------------------------------------*/
-/* [❗]                      [❗]                      [❗] */  
-/*                                                       */ 
-/*       |- [ ⚠ ] - CREDITOS DEL CODIGO - [ ⚠ ] -|      */
-/*     —◉ DESAROLLADO POR OTOSAKA:                       */
-/*     ◉ Otosaka (https://github.com/6otosaka9)          */
-/*     ◉ Número: wa.me/51993966345                       */
-/*                                                       */
-/*     —◉ FT:                                            */
-/*     ◉ BrunoSobrino (https://github.com/BrunoSobrino)  */
-/*                                                       */
-/* [❗]                      [❗]                      [❗] */
-/*-------------------------------------------------------*/
-import fetch from 'node-fetch'
-import axios from 'axios'
-import { Configuration, OpenAIApi } from 'openai'
+import fetch from 'node-fetch';
+import { Configuration, OpenAIApi } from 'openai';
+import translate from '@vitalets/google-translate-api';
+
 const configuration = new Configuration({ organization: global.openai_org_id, apiKey: global.openai_key });
 const openaiii = new OpenAIApi(configuration);
-let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw `*[❗] Insira uma solicitação ou pedido para usar a função de bate-papo gpt*\n\n*—◉ Exemplo de funções e ordens:*\n*◉ ${usedPrefix + command} Reflexão sobre a série Narcos na netflix*\n*◉ ${usedPrefix + command} Código JS para um jogo de cartas*`    
-try {
-let chgptdb = global.chatgpt.data.users[m.sender];
-chgptdb.push({ role: 'user', content: text });
-const config = { method: 'post', url: 'https://api.openai.com/v1/chat/completions', headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + global.openai_key }, data: JSON.stringify({ 'model': 'gpt-3.5-turbo', 'messages': [{ role: 'system', content: 'Você atuará como um WhatsApp Bot que foi criado por NeewJax, você será o The Hercules - Bot' }, ...chgptdb ]})}
-let response = await axios(config);
-chgptdb.push({ role: 'assistant', content: response.data.choices[0].message.content }) 
-m.reply(response.data.choices[0].message.content)
-} catch (efe1) {
-console.log(efe1)    
-try {
-const BotIA222 = await openaiii.createCompletion({ model: "text-davinci-003", prompt: text, temperature: 0.3, max_tokens: 4097, stop: ["Ai:", "Human:"], top_p: 1, frequency_penalty: 0.2, presence_penalty: 0, })
-m.reply(BotIA222.data.choices[0].text.trim())    
-} catch (efe2) {
-console.log(efe2)    
-try {
-let IA = await fetch(`https://api.amosayomide05.cf/gpt/?question=${text}&string_id=${m.sender}`)  
-let IAR = await IA.json()
-conn.sendMessage(m.chat, { text: `${IAR.response}`.trim() }, { quoted: m });
-} catch (efe) {
-console.log(efe)    
-try {   
-let rres = await fetch(`https://api.ibeng.tech/api/info/openai?text=${text}&apikey=tamvan`)
-let jjson = await rres.json()
-m.reply(jjson.data.data.trim())    
-} catch (efe3) {    
-console.log(efe3)
-try {
-let tioress22 = await fetch(`https://api.lolhuman.xyz/api/openai?apikey=${lolkeysapi}&text=${text}&user=${m.sender}`)
-let hasill22 = await tioress22.json()
-m.reply(`${hasill22.result}`.trim())         
-} catch (efe4) {   
-console.log(efe4)
-throw `*[❗] Erro... tente novamente!*`   
-}}}}}}
-handler.command = ['openai2', 'gpt', 'chatgpt2', 'ia2', 'robot2', 'Mystic', 'MysticBot']
-export default handler
+
+const handler = async (m, { conn, text, usedPrefix, command }) => {
+    if (usedPrefix == 'a' || usedPrefix == 'A') return;
+    if (!text) throw `*Diga algo para conversar com o chat gpt\n\n❏ Exemplo: \n❏ ${usedPrefix + command} Faça um texto romântico\n❏ ${usedPrefix + command}`
+    try {
+        conn.sendPresenceUpdate('composing', m.chat);
+        let gpt = await fetch(`https://delirius-api-oficial.vercel.app/api/ia2?text=${text}`);
+        let res = await gpt.json();
+        let translated = await translate(res.gpt, { to: 'pt' });
+        await m.reply(translated.text);
+    } catch {
+        try {
+            let gpt = await fetch(`https://delirius-api-oficial.vercel.app/api/chatgpt?q=${text}`);
+            let res = await gpt.json();
+            let translated = await translate(res.data, { to: 'pt' });
+            await m.reply(translated.text);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+}
+
+handler.command = /^(openai|chatgpt|ia|ai|openai2|chatgpt2|ia2)$/i;
+export default handler;
