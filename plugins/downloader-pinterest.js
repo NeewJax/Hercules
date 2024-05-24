470b581b0df720520fff7edacd5c15afb238b3c8
NeewJax <gabrielduarte.gomes09@gmail.com>
2024-05-23 23:56:04 -0300

    some changes on script

--- plugins/downloader-pinterest.js
+++ plugins/downloader-pinterest.js
@@ -1,13 +1,37 @@
-import { pinterest } from '@bochilteam/scraper'
-let handler = async(m, { conn, text, usedPrefix, command }) => {
-if (!text) throw `*[❗𝐈𝐍𝐅𝐎❗] Exemplo do uso do comando: ${usedPrefix + command} Minecraft*`
-const json = await pinterest(text)
-conn.sendFile(m.chat, json.getRandom(), 'error.jpg', `
-*Resultado da busca*
-${text}
-`.trim(), m)
-}
-handler.help = ['pinterest <keyword>']
-handler.tags = ['internet']
-handler.command = /^(pinterest)$/i
-export default handler
+import fetch from 'node-fetch';
+import puppeteer from 'puppeteer';
+import axios from 'axios';
+
+let handler = async (m, { conn, args, command, usedPrefix }) => {
+    if (!args[0]) throw `*[❗𝐈𝐍𝐅𝐎❗] Insira um link do pinterest. Exemplo: ${usedPrefix + command}* https://pin.it/4WESSVOI3`;
+    m.reply(`*✅ Fazendo o download, por favor aguarde um momento...*`);
+
+    try {
+        const browser = await puppeteer.launch({ args: ['--no-sandbox'] }); // using --no-sandbox to launch on aws instance
+        const page = await browser.newPage();
+        await page.goto(`https://delirius-api-oficial.vercel.app/api/pindl?url=${args[0]}`);
+
+        // Espera até que o elemento com o link de download esteja disponível na página
+        await page.waitForSelector('pre', { timeout: 20000 }); // Aumentando o tempo limite para 60 segundos
+
+        // Obtém o link de download diretamente do texto dentro do elemento 'pre'
+        const downloadLink = await page.$eval('pre', element => {
+            const json = JSON.parse(element.textContent);
+            // Acessa o primeiro elemento do array de downloads
+            return json.data.download[0].download;
+        });
+
+        await browser.close();
+
+        // Baixa o vídeo usando o link obtido
+        const response = await axios.get(downloadLink, { responseType: 'arraybuffer' });
+
+        // Envie o vídeo baixado para o usuário
+        await conn.sendFile(m.chat, Buffer.from(response.data), 'video.mp4', '', m);
+    } catch (error) {
+        console.error('Erro ao baixar vídeo:', error);
+        throw `ixe, conseguir baixar não viu...`;
+    }
+};
+handler.command = /^(pinterest|baixarpin|pin)$/i
+export default handler;
